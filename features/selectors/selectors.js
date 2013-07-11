6bd92a33ebb6ab7345ba80e0598cf9c63a9713c4
Simon Madine <simon@angryrobotzombie.com>
2013-07-11 23:34:11 +0200

    Adding jasmine

--- features/selectors/selectors.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	"main search field": ".single-topbar-search-field",
-	"main search form": ".single-topbar-search-form",
-	"main header": "#check-out-here-top h1",
-	"platforms menu": "#platforms",
-	"main link":"html > body#homepage > div#page-wrapper > div.section.classes-section > div.center-page div.class-tile div.meta-info > h3 > a"
-};
