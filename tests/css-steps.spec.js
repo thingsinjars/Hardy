8043e2ec431119c611889175b30524d78f36a1b5
Simon Madine <simon@angryrobotzombie.com>
2013-07-13 09:29:57 +0200

    Added placeholders for more tests

---
+++ tests/css-steps.spec.js
@@ -0,0 +1,28 @@
+/* CSS Steps Tests
+ */
+
+var CSSSteps, basedir;
+
+basedir = '../';
+
+var mockery = require('mockery');
+
+describe('CSS Steps: ', function() {
+
+	var fsMock, httpMock, child_processMock;
+
+	console.log = jasmine.createSpy('Console log');
+
+	beforeEach(function() {
+		console.log.reset();
+		mockery.registerAllowable(basedir + 'features/step_definitions/css.js');
+	});
+
+	describe('<Then> the <element> should have <property> of <value>', function() {
+
+		it('fails if the element selector is invalid', function() {
+			CSSSteps = require(basedir + 'features/step_definitions/css.js');
+		});
+	});
+
+});
---
+++ tests/css-utils.spec.js
@@ -0,0 +1,28 @@
+/* CSS Steps Tests
+ */
+
+var CSSUtils, basedir;
+
+basedir = '../';
+
+var mockery = require('mockery');
+
+describe('CSS Utilities: ', function() {
+
+	var fsMock, httpMock, child_processMock;
+
+	console.log = jasmine.createSpy('Console log');
+
+	beforeEach(function() {
+		console.log.reset();
+		mockery.registerAllowable(basedir + 'features/support/css-utils.js');
+	});
+
+	describe('<Then> the <element> should have <property> of <value>', function() {
+
+		it('fails if the element selector is invalid', function() {
+			CSSUtils = require(basedir + 'features/support/css-utils.js');
+		});
+	});
+
+});
