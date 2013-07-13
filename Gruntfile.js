0fd3ee789014ce2c06981333abd917df06a0a574
Simon Madine <simon@angryrobotzombie.com>
2013-07-13 14:51:52 +0200

    Moved tests and added Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    jasmine_node: {
+      specNameMatcher: "./tests/spec", // load only specs containing specNameMatcher
+      projectRoot: ".",
+      requirejs: false,
+      forceExit: true,
+      jUnit: {
+        report: false,
+        savePath: "./tests/reports/jasmine/",
+        useDotNotation: true,
+        consolidate: true
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-jasmine-node');
+
+  grunt.registerTask('test', 'jasmine_node');
+};
