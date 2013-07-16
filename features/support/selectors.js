335e2479e85470d34c38f59108b168d55dc64393
Simon Madine <simon@angryrobotzombie.com>
2013-07-16 20:51:30 +0200

    Moving things around for sturdier testability

--- features/support/selectors.js
+++ features/support/selectors.js
@@ -1,6 +1,6 @@
 module.exports = function(elementName, testpath) {
 	var path = require('path');
-	testpath = testpath || testpath;
+	testpath = testpath || process.env['TESTPATH'];
 
 	if(testpath) {
 		return require(path.resolve(testpath) + '/selectors.js')[elementName] || elementName;
