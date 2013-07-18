b9d105aad19b4b3ab1d78b4db4a93acaf9372d5a
Simon Madine <simon@thingsinjars.com>
2013-07-18 13:49:30 +0200

    More tests testing the tests.

--- tests/spec/css-steps.spec.js
+++ tests/spec/css-steps.spec.js
@@ -19,37 +19,45 @@ describe('CSS Steps: ', function() {
 		};
 		utilsMock = {
 			isColor: jasmine.createSpy('initialising imageTest').andReturn(true),
-			toRgba: jasmine.createSpy('convert to RGBa').andCallFake(function(a) {return a;}),
-			normalizeString: jasmine.createSpy('normalizing CSS values').andCallFake(function(a) {return a;})
+			toRgba: jasmine.createSpy('convert to RGBa').andCallFake(function(a) {
+				return a;
+			}),
+			normalizeString: jasmine.createSpy('normalizing CSS values').andCallFake(function(a) {
+				return a;
+			})
 		};
 		assertMock = {
 			equal: jasmine.createSpy('assert equal').andCallFake(function(a, b, message) {
-				if(a === b) {
+				if (a === b) {
 					return true;
 				} else {
-					throw new Error('assertion error')
+					throw new Error('Assertion error')
 				}
 			}),
 			ok: jasmine.createSpy('assert ok').andCallFake(function(a, message) {
-				if(a) {
+				if (a) {
 					return a;
 				} else {
-					throw new Error('assertion error')
+					throw new Error('Assertion error')
 				}
 			})
 		};
+		assertMock.equal.reset();
+		assertMock.ok.reset();
+
 		selectorsMock = function() {
 			return 'mock > selector';
 		};
+		
 		cucumberThens = [];
 		cucumberGivens = [];
 		cucumberMock = {
 			'Then': jasmine.createSpy('Then').andCallFake(function(matcher, callback) {
 				cucumberThens[matcher] = callback;
-					}),
+			}),
 			'Given': jasmine.createSpy('Given').andCallFake(function(matcher, callback) {
 				cucumberGivens[matcher] = callback;
-					}),
+			}),
 			setWindowSize: jasmine.createSpy('setting window size'),
 			getCssProperty: jasmine.createSpy('measure CSS property from browser instance').andCallFake(function(elementSelector, property, callback) {
 				callback(null, 10);
@@ -58,6 +66,7 @@ describe('CSS Steps: ', function() {
 
 		callbackMock = function() {};
 		callbackMock.fail = jasmine.createSpy('Callback fail');
+		callbackMock.fail.reset();
 
 		mockery.registerAllowable(basedir + 'features/step_definitions/css.js');
 
@@ -88,23 +97,17 @@ describe('CSS Steps: ', function() {
 			expect(cucumberMock.Then).toHaveBeenCalledWith(/^"([^"]*)" should look the same as before$/, jasmine.any(Function));
 		});
 	});
-	describe('running CSS test steps', function() {
-		it('should set window size', function() {
+	describe('Test step', function() {
+		it('"should set window size"', function() {
 			cucumberGivens['/^the window size is "([^"]*)" by "([^"]*)"$/'].apply(cucumberMock);
 			expect(cucumberMock.setWindowSize).toHaveBeenCalled();
 		});
 
-		it('<selector> should have <property> of <value>', function() {
-			cucumberThens['/^"([^"]*)" should have "([^"]*)" of "([^"]*)"$/'].call(cucumberMock, 'mockElementName', 'mockProperty', 'mockValue', callbackMock);
-			expect(cucumberMock.getCssProperty).toHaveBeenCalledWith('mock > selector', 'mockProperty', jasmine.any(Function));
-			expect(assertMock.equal).toHaveBeenCalled();
-		});
-
-		describe('<selector> should have <property> of <value>', function() {
+		describe('"<selector> should have <property> of <value>"', function() {
 			it('should match exact colours', function() {
 				cucumberMock.getCssProperty = jasmine.createSpy('measure CSS property from browser instance').andCallFake(function(elementSelector, property, callback) {
 					callback(null, 'rgb(10, 10, 10)');
-				})
+				});
 
 				cucumberThens['/^"([^"]*)" should have "([^"]*)" of "([^"]*)"$/'].call(cucumberMock, 'mockElementName', 'mockProperty', 'rgb(10, 10, 10)', callbackMock);
 				expect(cucumberMock.getCssProperty).toHaveBeenCalledWith('mock > selector', 'mockProperty', jasmine.any(Function));
@@ -114,7 +117,7 @@ describe('CSS Steps: ', function() {
 			it('should not match different colours', function() {
 				cucumberMock.getCssProperty = jasmine.createSpy('measure CSS property from browser instance').andCallFake(function(elementSelector, property, callback) {
 					callback(null, 'rgb(10, 10, 100)');
-				})
+				});
 
 				cucumberThens['/^"([^"]*)" should have "([^"]*)" of "([^"]*)"$/'].call(cucumberMock, 'mockElementName', 'mockProperty', 'rgb(10, 10, 10)', callbackMock);
 				expect(cucumberMock.getCssProperty).toHaveBeenCalledWith('mock > selector', 'mockProperty', jasmine.any(Function));
@@ -123,11 +126,28 @@ describe('CSS Steps: ', function() {
 			});
 		});
 
-		it('<selector> should have <property> of <comparator> than <value>', function() {
-			cucumberThens['/^"([^"]*)" should have "([^"]*)" of ([^"]*) than "([^"]*)"$/'].call(cucumberMock, 'mockElementName', 'mockProperty', 'mockComparator', '9', callbackMock);
-			expect(cucumberMock.getCssProperty).toHaveBeenCalled();
-			expect(assertMock.ok).toHaveBeenCalled();
-			expect(callbackMock.fail).not.toHaveBeenCalled();
+		describe('"<selector> should have <property> of <comparator> than <value>"', function() {
+			it('should pass when the comparison is true', function() {
+				cucumberMock.getCssProperty = jasmine.createSpy('measure CSS property from browser instance').andCallFake(function(elementSelector, property, callback) {
+					callback(null, 10);
+				});
+
+
+				cucumberThens['/^"([^"]*)" should have "([^"]*)" of ([^"]*) than "([^"]*)"$/'].call(cucumberMock, 'mockElementName', 'mockProperty', 'less', '9', callbackMock);
+				expect(cucumberMock.getCssProperty).toHaveBeenCalled();
+				expect(assertMock.ok).toHaveBeenCalled();
+				expect(callbackMock.fail).toHaveBeenCalled();
+			});
+			it('should fail when the comparison is false', function() {
+				cucumberMock.getCssProperty = jasmine.createSpy('measure CSS property from browser instance').andCallFake(function(elementSelector, property, callback) {
+					callback(null, 10);
+				});
+
+				cucumberThens['/^"([^"]*)" should have "([^"]*)" of ([^"]*) than "([^"]*)"$/'].call(cucumberMock, 'mockElementName', 'mockProperty', 'greater', '9', callbackMock);
+				expect(cucumberMock.getCssProperty).toHaveBeenCalled();
+				expect(assertMock.ok).toHaveBeenCalled();
+				expect(callbackMock.fail).not.toHaveBeenCalled();
+			});
 		});
 
 	});
