72a7ae6b4760f1a2e9f95d649af6a894ae5d6baf
Simon Madine <simon@thingsinjars.com>
2013-07-19 23:36:26 +0200

    Grunt task for completely headless tests

--- tests/acceptance/phantom.sh
+++ tests/acceptance/phantom.sh
@@ -1,33 +1,6 @@
 #!/bin/bash
 EXIT_STATUS=0;
 
-# Run the tests in Firefox
-echo -n "Running tests in Firefox..."
-ffout="`../../bin/hardy .`"
-
-needle='# Scenario: Missing page'
-if [[ "$ffout" != *"$needle"* ]]; then
-	EXIT_STATUS=1;
-fi
-needle='# Scenario: Missing element'
-if [[ "$ffout" != *"$needle"* ]]; then
-	EXIT_STATUS=1;
-fi
-needle='# Scenario: Failing comparison'
-if [[ "$ffout" != *"$needle"* ]]; then
-	EXIT_STATUS=1;
-fi
-needle='8 scenarios (3 failed, 5 passed)'
-if [[ "$ffout" != *"$needle"* ]]; then
-	EXIT_STATUS=1;
-fi
-
-if [ "$EXIT_STATUS" -eq 0 ]; then
-	echo "..passed"
-else
-	echo "..failed"
-fi
-
 # Run the tests in our PhantomJS
 command -v phantomjs >/dev/null 2>&1 || { echo >&2 "PhantomJS not installed. Skipping PhantomJS tests."; exit 1; }
 echo -n "Running tests in PhantomJS..."
