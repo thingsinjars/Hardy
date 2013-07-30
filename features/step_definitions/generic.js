bb50b91496616bd5f245b0a975a96c6fb2f703ee
Simon Madine <simon@thingsinjars.com>
2013-07-30 09:49:25 +0200

    Making URL matcher more generic

--- features/step_definitions/generic.js
+++ features/step_definitions/generic.js
@@ -5,7 +5,7 @@ module.exports = function () {
     var windowSizeIs;
 
     /* "<Given> I visit <url>" */
-    this.Given(/^I visit "(https?:\/\/.*\..*)"$/, function (url, callback) {
+    this.Given(/^I visit "([^"]*)"$/, function (url, callback) {
         try {
             this.init().url(url, callback);
         } catch(o_O) {
@@ -39,25 +39,4 @@ module.exports = function () {
         this.submitForm(selector, callback);
     });
 
-    /* "<Then> I should see <text> in the element <selector>" */
-    this.Then(/^I should see '(.*)' in the element '(.*)'$/, function (text, selector, callback) {
-        selector = selectors(selector);
-
-        this.waitFor(selector, 1000, function (result) {
-            // if (result.status !== 0) return callback.fail(new Error('Element ' + inputId + ' was not found after 2s'));
-
-            // this.getText(inputId, function(text) {
-            //     text = ('' + text.value).match(/^We've found ([\d,]+) repository results$/);
-            //     text = parseInt(text[1].replace(/,/g, ''), 10);
-
-            //     // Fail this test if the search results are null
-            //     if (text === 0 || isNaN(text)) return callback.fail(new Error('Expected to see some results, but saw 0 (captured result count is ' + number + ')'));
-
-            //     // Pass and say about it.
-            //     console.log('(Saw ' + text + ' results)');
-            //     return callback();
-
-            // });
-        });
-    });
 };
