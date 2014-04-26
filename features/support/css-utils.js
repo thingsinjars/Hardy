e6ecf77bca914fec1f411c7a9e42c59b5278ad97
Peter Müller <munter@fumle.dk>
2014-04-26 13:58:11 +0200

    Remove color detection and conversion tests, since those are covered in external color library now

--- features/support/css-utils.js
+++ features/support/css-utils.js
@@ -1,9 +1,5 @@
 module.exports = (function() {
-    // This holds a few handy utilities for things like converting colors.
-    //
-    // When measured via computedStyles, most browsers return rgba
-    // so we'll need to convert everything to rgba to measure it
-    // without forcing all story files to be written with rgba in mind.
+    // This holds a few handy value comparison utilities.
     // console.log('CSS Utils Steps Loaded');
     // this.World = require('../support/world.js');
 
